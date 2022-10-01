f21843369a88c470c83c23865d3f559a0d64b178
Anton <anton.omelianenko533@gmail.com>
2022-10-01 19:37:25 +0300

    add image tree

--- lib/widgets/main_screen/main_screen_widget.dart
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-// import 'package:themoviedb/widgets/images_widget.dart';
+// import 'package:flutter/services.dart';
+import 'package:themoviedb/widgets/images_widget.dart';
 
 class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({super.key});
@@ -13,10 +13,9 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
   int _selectedTab = 0;
 
   static const List<Widget> _widgetOptions = <Widget>[
-    // Exampel(),
-    Text(
-      'Index 0: Home',
-    ),
+    Example(),
+    // Text(
+    //   'Index 0: Home'),
     Text(
       'Index 1: Films',
     ),
@@ -63,54 +62,54 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
   }
 }
 
-// Exampel TextField
+// Example TextField
 
-// class Exampel extends StatefulWidget {
-//   const Exampel({super.key});
+class Example extends StatefulWidget {
+  const Example({super.key});
 
-//   @override
-//   State<Exampel> createState() => _ExampelState();
-// }
+  @override
+  State<Example> createState() => _ExampleState();
+}
 
-// class _ExampelState extends State<Exampel> {
-//   final myTextController = TextEditingController();
+class _ExampleState extends State<Example> {
+  final myTextController = TextEditingController();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     // const imageTree = AppImages.tree;
-//     // const imageSun = AppImages.sun;
-//     return ListView(
-//       children: [
-//         Center(
-//           child: Padding(
-//             padding: const EdgeInsets.all(40),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 ElevatedButton(
-//                     onPressed: myTextController.clear,
-//                     child: const Text('Clear')),
-//                 const SizedBox(height: 30),
-//                 TextField(
-//                   controller: myTextController,
-//                   decoration: InputDecoration(
-//                     border: OutlineInputBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                     ),
-//                   ),
-//                 ),
-//                 const Image(
-//                   image: AppImages.tree,
-//                 ),
-//                 const SizedBox(height: 30),
-//                 const Image(image: AppImages.funSun),
-//                 const SizedBox(height: 30),
-//                 const Image(image: AppImages.shishki)
-//               ],
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    // const imageTree = AppImages.tree;
+    // const imageSun = AppImages.sun;
+    return ListView(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: myTextController.clear,
+                    child: const Text('Clear')),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: myTextController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const Image(
+                  image: AppImages.tree,
+                ),
+                const SizedBox(height: 30),
+                const Image(image: AppImages.funSun),
+                const SizedBox(height: 30),
+                const Image(image: AppImages.shishki)
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
